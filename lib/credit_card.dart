acb2555ee289ccb61c4c7d4a75bda4b1161df00a
yassine ouarzazi <ouarzaziyassine@gmail.com>
2019-07-10 23:32:21 +0100

    6451

--- lib/credit_card.dart
+++ lib/credit_card.dart
@@ -20,9 +20,24 @@ AnimationController _animationController;
       @override
       void initState() {
         c_number_controller.addListener((){
-          if(c_number_controller.text.length == 4 || c_number_controller.text.length  == 9 || c_number_controller.text.length == 14)
-            c_number_controller.text += "-";
-            
+          setState(() {
+                      
+                    });
+        });
+        c_name_controller.addListener((){
+          setState(() {
+                      
+                    });
+        });
+        c_exper_controller.addListener((){
+          setState(() {
+                      
+                    });
+        });
+        c_cvc_controller.addListener((){
+          setState(() {
+                      
+                    });
         });
         super.initState();
    
@@ -34,6 +49,9 @@ AnimationController _animationController;
   void dispose() {
     _animationController.dispose();
     c_number_controller.dispose();
+    c_name_controller.dispose();
+    c_exper_controller.dispose();
+    c_cvc_controller.dispose();
     super.dispose();
   }
 
@@ -103,9 +121,49 @@ AnimationController _animationController;
               padding: EdgeInsets.all(10),
               child: FlipView(
                     animationController: _curvedAnimation,
-                    front: _buildCard('A', () => _flip(true)),
-                    back: _buildCard('B', () => _flip(false)),
+                    front: _buildCard(true, () => _flip(true)),
+                    back: _buildCard(false, () => _flip(false)),
                 )
+            ),
+
+            Container(
+              padding: EdgeInsets.all(20),
+              child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Text("Crad Number: ", style: TextStyle(
+                      fontWeight: FontWeight.bold
+                    ),),
+                    Text(c_number_controller.text)
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Text("Holder Name: ", style: TextStyle(
+                      fontWeight: FontWeight.bold
+                    ),),
+                    Text(c_name_controller.text)
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Text("Experation: ", style: TextStyle(
+                      fontWeight: FontWeight.bold
+                    ),),
+                    Text(c_exper_controller.text)
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Text("CVC: ", style: TextStyle(
+                      fontWeight: FontWeight.bold
+                    ),),
+                    Text(c_cvc_controller.text)
+                  ],
+                )
+              ],
+            ),
             )
           ],
         ),
@@ -116,9 +174,12 @@ AnimationController _animationController;
       }
 
       TextEditingController c_number_controller = TextEditingController();
+      TextEditingController c_name_controller = TextEditingController();
+      TextEditingController c_exper_controller = TextEditingController();
+      TextEditingController c_cvc_controller = TextEditingController();
 
 
-      Widget _buildCard(String title, GestureTapCallback onTap) {
+      Widget _buildCard(bool front, GestureTapCallback onTap) {
     return AspectRatio(
       aspectRatio: 1.586,
       child: Card(
@@ -146,7 +207,7 @@ AnimationController _animationController;
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: <Widget>[
-                  Container(
+                  front? Container(
                     margin: EdgeInsets.only(top: 10, right: 10),
                     alignment: Alignment.centerRight,
                     width: 90,
@@ -156,8 +217,16 @@ AnimationController _animationController;
                         image: AssetImage("assets/images/mastercard_logo.png")
                       )
                     ),
+                  ) :
+                  Container(
+                    margin: EdgeInsets.only(top: 20),
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.black
+                    ),
                   ),
-                  Form(
+                  (front)?
+                    Form(
                     child: Container(
                       padding: EdgeInsets.only(left: 10, right: 10),
                       child: Column(
@@ -168,6 +237,7 @@ AnimationController _animationController;
                             style: TextStyle(
                               color: Colors.white
                             ),
+                            cursorColor: Colors.white,
                             controller: c_number_controller,
                                 decoration: new InputDecoration(
                                   labelText: "Card Number",
@@ -182,9 +252,165 @@ AnimationController _animationController;
                                     return null;
                                   }
                                 },
+                                inputFormatters: [
+                                  MaskedTextInputFormatter(
+                                    mask: 'xxxx-xxxx-xxxx-xxxx',
+                                    separator: '-',
+                                  ),
+                                ],
                                 keyboardType: TextInputType.number,
                         ),
                         ),
+                        Row(
+                          children: <Widget>[
+                            
+                        //--------------------------------------
+                          Expanded(
+                            flex: 2,
+                            child: Container(
+                          padding: EdgeInsets.only(bottom: 10, right: 10),
+                          child: new TextFormField(
+                            controller: c_name_controller,
+                            style: TextStyle(
+                              color: Colors.white
+                            ),
+                            cursorColor: Colors.white,
+                                decoration: new InputDecoration(
+                                  labelText: "Holder Name",
+                                  labelStyle: TextStyle(
+                      ),
+                                  fillColor: Colors.white,
+                                ),
+                                validator: (val) {
+                                  if(val.length==0) {
+                                    return "Title cannot be empty";
+                                  }else{
+                                    return null;
+                                  }
+                                },
+                                inputFormatters: [
+                                  // MaskedTextInputFormatter(
+                                  //   mask: 'xxx',
+                                  //   separator: '',
+                                  // ),
+                                ],
+                                keyboardType: TextInputType.text,
+                        ),
+                        ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Container(
+                          padding: EdgeInsets.only(bottom: 10, right: 10),
+                          child: new TextFormField(
+                            controller: c_exper_controller,
+                            style: TextStyle(
+                              color: Colors.white
+                            ),
+                            cursorColor: Colors.white,
+                                decoration: new InputDecoration(
+                                  labelText: "Expiration",
+                                  labelStyle: TextStyle(
+                      ),
+                                  fillColor: Colors.white,
+                                ),
+                                validator: (val) {
+                                  if(val.length==0) {
+                                    return "Title cannot be empty";
+                                  }else{
+                                    return null;
+                                  }
+                                },
+                                inputFormatters: [
+                                  MaskedTextInputFormatter(
+                                    mask: 'xx/xx',
+                                    separator: '/',
+                                  ),
+                                ],
+                                keyboardType: TextInputType.number,
+                        ),
+                        ),
+                           
+                          ),
+                        
+                          ],
+                        )
+                      ],
+                    ),
+                    )
+                  )
+
+                  :
+
+
+                  Form(
+                    child: Container(
+                      padding: EdgeInsets.only(left: 10, right: 10),
+                      child: Column(
+                      children: <Widget>[
+                        
+                        Row(
+                          children: <Widget>[
+                            
+                        //--------------------------------------
+                   
+                          Expanded(
+                            flex: 1,
+
+                            child: Container(
+                          padding: EdgeInsets.only(bottom: 10, right: 10),
+                          child: new TextFormField(
+                            controller: c_cvc_controller,
+
+                            style: TextStyle(
+                              
+                              color: Colors.white
+                            ),
+                            cursorColor: Colors.white,
+                                decoration: new InputDecoration(
+                                  labelText: "CVC",
+                                  labelStyle: TextStyle(
+                      ),
+                                  fillColor: Colors.white,
+                                ),
+                                validator: (val) {
+                                  if(val.length==0) {
+                                    return "Title cannot be empty";
+                                  }else{
+                                    return null;
+                                  }
+                                },
+                                inputFormatters: [
+                                  MaskedTextInputFormatter(
+                                    mask: 'xxx',
+                                    separator: '',
+                                  ),
+                                ],
+                                keyboardType: TextInputType.number,
+                        ),
+                        ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Container(),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Container(),
+                          )
+                          ],
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: 10, right: 10),
+                          alignment: Alignment.bottomRight,
+                          width: 90,
+                          height: 40,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage("assets/images/mastercard_logo.png")
+                            )
+                          ),
+                        )
                       ],
                     ),
                     )
@@ -198,3 +424,33 @@ AnimationController _animationController;
     );
   }
     }
+
+
+
+    class MaskedTextInputFormatter extends TextInputFormatter {
+  final String mask;
+  final String separator;
+
+  MaskedTextInputFormatter({
+    @required this.mask,
+    @required this.separator,
+  }) { assert(mask != null); assert (separator != null); }
+
+  @override
+  TextEditingValue formatEditUpdate(TextEditingValue oldValue, TextEditingValue newValue) {
+    if(newValue.text.length > 0) {
+      if(newValue.text.length > oldValue.text.length) {
+        if(newValue.text.length > mask.length) return oldValue;
+        if(newValue.text.length < mask.length && mask[newValue.text.length - 1] == separator) {
+          return TextEditingValue(
+            text: '${oldValue.text}$separator${newValue.text.substring(newValue.text.length-1)}',
+            selection: TextSelection.collapsed(
+              offset: newValue.selection.end + 1,
+            ),
+          );
+        }
+      }
+    }
+    return newValue;
+  }
+}
