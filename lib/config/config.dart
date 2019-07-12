e3a1b40729dd76ccb9d6353fe210e9bd7046f394
yassine ouarzazi <ouarzaziyassine@gmail.com>
2019-07-12 02:28:11 +0100

    561561568

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,7 +1,8 @@
 class AppConfig
 {
   // static String ip = "https://recyclers.herokuapp.com";
-  static String ip = "http://10.12.4.14:1337";
+  static String ip = "http://142.93.233.231";
+  // static String ip = "http://10.12.4.14:1337";
   // static String ip = "http://192.168.1.107:1337";
   // static String ip = "http://10.30.244.215:1337";
 }
--- lib/payment.dart
+++ lib/payment.dart
@@ -25,6 +25,13 @@ class _PaymentScreenState extends State<PaymentScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+            showDialog(context: context,builder: (context) => CardsDialog());
+        },
+        backgroundColor: Color(0xff00b661),
+      ),
       appBar: AppBar(
         // actions: <Widget>[
         //   FlatButton(
@@ -55,7 +62,7 @@ class _PaymentScreenState extends State<PaymentScreen> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-              Text("Select a Payment Method", style: TextStyle(
+              Text("Add a Payment Method", style: TextStyle(
                     fontFamily: "Oswald",
                     fontSize: 25,
                     color: Colors.black
@@ -73,162 +80,7 @@ class _PaymentScreenState extends State<PaymentScreen> {
               ],
             ),
             ),
-            Container(
-              margin: EdgeInsets.only(left: 10, right: 10, bottom: 10),
-              child: InkWell(
-                onTap: (){
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => CreditCardScreen(service: "MasterCard",)),
-                      );
-                },
-                child: Container(
-                  padding: EdgeInsets.only(left: 10, right: 10),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(3)),
-                color: Colors.white,
-                boxShadow: [
-                              BoxShadow(color: Colors.black26, offset: Offset.zero, blurRadius: 2, spreadRadius: 0),
-                            ],
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Container(
-                    height: 50,
-                    width: 50,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("assets/images/credit-cards_mastercard.png")
-                      )
-                    ),
-                  ),
-                  Text(
-                    "Mastercard",
-                    style: TextStyle(
-                      fontSize: 20
-                    ),
-                  )
-                ],
-              ),
-              )
-              ),
-            ),
-Container(
-              margin: EdgeInsets.only(left: 10, right: 10, bottom: 10),
-              child: InkWell(
-                onTap: (){
-
-                },
-                child: Container(
-                  padding: EdgeInsets.only(left: 10, right: 10),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(3)),
-                color: Colors.white,
-                boxShadow: [
-                              BoxShadow(color: Colors.black26, offset: Offset.zero, blurRadius: 2, spreadRadius: 0),
-                            ],
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Container(
-                    height: 50,
-                    width: 50,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("assets/images/credit-cards_visa.png")
-                      )
-                    ),
-                  ),
-                  Text(
-                    "Visa",
-                    style: TextStyle(
-                      fontSize: 20
-                    ),
-                  )
-                ],
-              ),
-              )
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(left: 10, right: 10, bottom: 10),
-              child: InkWell(
-                onTap: (){
-
-                },
-                child: Container(
-                  padding: EdgeInsets.only(left: 10, right: 10),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(3)),
-                color: Colors.white,
-                boxShadow: [
-                              BoxShadow(color: Colors.black26, offset: Offset.zero, blurRadius: 2, spreadRadius: 0),
-                            ],
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Container(
-                    height: 50,
-                    width: 50,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("assets/images/credit-cards_paypal.png")
-                      )
-                    ),
-                  ),
-                  Text(
-                    "Paypal",
-                    style: TextStyle(
-                      fontSize: 20
-                    ),
-                  )
-                ],
-              ),
-              )
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(left: 10, right: 10, bottom: 10),
-              child: InkWell(
-                onTap: (){
-
-                },
-                child: Container(
-                  padding: EdgeInsets.only(left: 10, right: 10),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(3)),
-                color: Colors.white,
-                boxShadow: [
-                              BoxShadow(color: Colors.black26, offset: Offset.zero, blurRadius: 2, spreadRadius: 0),
-                            ],
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Container(
-                    height: 50,
-                    width: 50,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("assets/images/credit-cards_bitcoin.png")
-                      )
-                    ),
-                  ),
-                  Text(
-                    "Bitcoin",
-                    style: TextStyle(
-                      fontSize: 20
-                    ),
-                  )
-                ],
-              ),
-              )
-              ),
-            ),
-            
+           
 
           Container(
             margin: EdgeInsets.all(20),
@@ -251,4 +103,240 @@ Container(
        
     );
   }
+}
+
+  class CardsDialog extends StatefulWidget {
+      CardsDialog({Key key}) : super(key: key);
+      @override
+      State<StatefulWidget> createState() => CardsDialogState();
+    }
+
+class CardsDialogState extends State<CardsDialog>
+        with SingleTickerProviderStateMixin{
+AnimationController controller;
+      Animation<double> scaleAnimation;
+ @override
+      void initState() {
+        super.initState();
+        
+        controller =
+            AnimationController(vsync: this, duration: Duration(milliseconds: 450));
+        scaleAnimation =
+            CurvedAnimation(parent: controller, curve: Curves.elasticInOut);
+
+        controller.addListener(() {
+          setState(() {});
+        });
+
+        controller.forward();
+      }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+          child: Material(
+            color: Colors.transparent,
+            child: ScaleTransition(
+              scale: scaleAnimation,
+              child: Wrap(
+                children: <Widget>[
+                  Container(
+                margin: EdgeInsets.all(20.0),
+                  padding: EdgeInsets.only(left: 5, right: 5, top: 15, bottom: 15),
+                  decoration: ShapeDecoration(
+                      color: Color(0xf100b661),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15.0))),
+                  child: Column(
+                    children: <Widget>[
+                     
+                        Container(
+              margin: EdgeInsets.only(left: 10, right: 10, bottom: 10),
+              child: InkWell(
+                onTap: (){
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => CreditCardScreen(service: "MasterCard",)),
+                      );
+                },
+                child: Container(
+                  padding: EdgeInsets.only(left: 10, right: 10),
+              decoration: BoxDecoration(
+                
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.only(right: 10),
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("assets/images/credit-cards_mastercard.png")
+                      )
+                    ),
+                  ),
+                  Text(
+                    "Mastercard",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18
+                    ),
+                  )
+                    ]
+                  ),
+                  Icon(Icons.keyboard_arrow_right, color: Colors.white,)                  
+                ],
+              ),
+              )
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 20, right: 20),
+              child: Divider(),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 10, right: 10, bottom: 10),
+              child: InkWell(
+                onTap: (){
+
+                },
+                child: Container(
+                  padding: EdgeInsets.only(left: 10, right: 10),
+              decoration: BoxDecoration(
+                
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.only(right: 10),
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("assets/images/credit-cards_visa.png")
+                      )
+                    ),
+                  ),
+                  Text(
+                    "Visa",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18
+                    ),
+                  )
+                    ]
+                  ),
+                  Icon(Icons.keyboard_arrow_right, color: Colors.white,)                  
+                ],
+              ),
+              )
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 20, right: 20),
+              child: Divider(),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 10, right: 10, bottom: 10),
+              child: InkWell(
+                onTap: (){
+
+                },
+                child: Container(
+                  padding: EdgeInsets.only(left: 10, right: 10),
+              decoration: BoxDecoration(
+                
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.only(right: 10),
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("assets/images/credit-cards_paypal.png")
+                      )
+                    ),
+                  ),
+                  Text(
+                    "Paypal",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18
+                    ),
+                  )
+                    ]
+                  ),
+                  Icon(Icons.keyboard_arrow_right, color: Colors.white,)                  
+                ],
+              ),
+              )
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 20, right: 20),
+              child: Divider(),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 10, right: 10),
+              child: InkWell(
+                onTap: (){
+
+                },
+                child: Container(
+                  padding: EdgeInsets.only(left: 10, right: 10),
+              decoration: BoxDecoration(
+                
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.only(right: 10),
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("assets/images/credit-cards_bitcoin.png")
+                      )
+                    ),
+                  ),
+                  Text(
+                    "Bitcoin",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18
+                    ),
+                  )
+                    ]
+                  ),
+                  Icon(Icons.keyboard_arrow_right, color: Colors.white,)                  
+                ],
+              ),
+              )
+              ),
+            ),
+                    ],
+                  )),
+                ],
+              )
+            ),
+          ),
+        );
+  }
+
 }
